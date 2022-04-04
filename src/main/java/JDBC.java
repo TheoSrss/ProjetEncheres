480da608ef1f69dec89ec4ac06c66e0acfcf7036
TheoSourisseau <tsourisseau@bourgoin-expertise.fr>
2022-04-04 11:17:40 +0200

    jdbc

---
+++ src/main/java/JDBC.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+
+public class JDBC {
+
+    public static void main(String[] args) throws SQLException {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/encheres", "root", "");
+
+            Statement statement = connection.createStatement();
+            ResultSet re = statement.executeQuery("select * from user");
+
+            while (re.next()) {
+                System.out.println(re.getString("username"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
